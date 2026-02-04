3ac5196200f3f1adbb292cb5801bb77a180aebc5
powizak <powizak@gmail.com>
2026-02-04 21:53:50 +0100

    feat: integrate Capacitor for Android and iOS support

---
+++ android/app/src/main/java/com/bezkomprese/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.bezkomprese.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.bezkomprese.app',
+  appName: 'Bez Komprese',
+  webDir: 'dist'
+};
+
+export default config;
